b3d9d2d91de66c3163bfe6c5c4f86da4f7819bf0
YeongSang Jeon <wjsdudtkd95@gmail.com>
2022-01-21 23:18:46 +0900

    fix(api): 반환타입 수정

--- domain/src/main/java/com/joseph/domain/interact/MovieRepository.kt
+++ domain/src/main/java/com/joseph/domain/interact/MovieRepository.kt
@@ -1,8 +1,9 @@
 package com.joseph.domain.interact
 
 import com.joseph.domain.model.MovieListEntity
+import kotlinx.coroutines.flow.Flow
 
 
 interface MovieRepository {
-    fun fetchUpComingMovieList(page: Int, language: String): MovieListEntity
+    fun fetchUpComingMovieList(page: Int, language: String): Flow<MovieListEntity>
 }
