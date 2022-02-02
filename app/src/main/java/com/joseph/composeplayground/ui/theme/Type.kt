1c5f5b49d91fb45712637707cc373b6c2023651b
YeongSang Jeon <wjsdudtkd95@gmail.com>
2022-02-02 12:26:03 +0900

    feat(base): add assets

--- app/src/main/java/com/joseph/composeplayground/ui/theme/Type.kt
+++ app/src/main/java/com/joseph/composeplayground/ui/theme/Type.kt
@@ -2,15 +2,22 @@ package com.joseph.composeplayground.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.joseph.composeplayground.R
+
+val Suit = FontFamily(
+    Font(R.font.suit_bold, FontWeight.Bold),
+    Font(R.font.suit_regular, FontWeight.Normal),
+    Font(R.font.suit_light, FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
     /* Other default text styles to override
